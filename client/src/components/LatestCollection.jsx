4fc757042e47e68210aa84d684929c49ae4e1f3e
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-08-27 11:33:33 +0600

    Best Seller component is done

--- client/src/components/LatestCollection.jsx
+++ client/src/components/LatestCollection.jsx
@@ -9,7 +9,7 @@ const LatestCollection = () => {
 
     useEffect(() => {
         setLatestProducts(products.slice(0, 10));
-        console.log(latestProducts)
+        
     }, [products])
 
     return (
@@ -23,7 +23,7 @@ const LatestCollection = () => {
             <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 gap-y-6'>
                 {
                     latestProducts.map((product, index)=>(
-                        <ProductItem key={index} id={product?.id} image={product?.image} name={product?.name} price={product.price} />
+                        <ProductItem key={index} id={product?._id} image={product?.image} name={product?.name} price={product.price} />
                     ))
                 }
             </div>
