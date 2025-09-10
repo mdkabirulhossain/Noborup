b1a0290507bfae2c10f6df9340da4898c78f197c
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-09-10 13:43:46 +0600

    Database is connected

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
+import connectDB from './config/mongodb.js';
 
 
 //App config
 const app = express();
 const port = process.env.PORT || 5000;
 
+//Connect Database
+ connectDB()
 
+//Middlewares
+app.use(express.json());
+app.use(cors());
+
+// api endpoints 
 app.get('/', (req, res)=>{
     res.send("Hello");
 })
