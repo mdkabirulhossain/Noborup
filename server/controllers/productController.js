e41b48c0d24c2e15994b708718f71fea92e0314b
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-09-11 15:56:47 +0600

    Write list product controller function

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -49,6 +49,14 @@ const addProduct = async(req, res)=>{
 }
 //function for list Product
 const listProduct = async(req, res)=>{
+    try{
+
+        const products = await productModel.find({});
+        res.json({success:true, products})
+    }catch(error){
+        console.log(error);
+        res.json({success:false, message: error.message})
+    }
 
 }
 //function for removing Product
