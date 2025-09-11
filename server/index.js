405760161d0fe8ed7b291ef8ea3a5519b494b5f6
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-09-11 14:54:57 +0600

    Write add product controller function

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js';
 import { connectCloudinary } from './config/cloudinary.js';
 import userRouter from './routes/userRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 
 //App config
@@ -20,7 +21,11 @@ app.use(express.json());
 app.use(cors());
 
 // api endpoints 
-app.use('/api/user', userRouter)
+//user routes
+app.use('/api/user', userRouter);
+
+//product routes
+app.use('/api/product', productRouter);
 
 
 app.get('/', (req, res)=>{
