465e2c21498cd97e9d1159e4c0f5b022d5d6cb51
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-09-13 23:36:34 +0600

    Created sidebar component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-      <Navbar />
+      <div >
+        <Navbar />
+        <hr />
+        <div className='flex w-full'>
+          <Sidebar />
+        </div>
+      </div>
     </div>
   );
 };
