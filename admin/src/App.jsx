1b1e6f135dc4bd1f998986d3c3fd7ddd5892ff07
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-09-14 02:16:15 +0600

    Admin login page component done

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import { Route, Routes } from 'react-router';
 import Add from './pages/add';
 import List from './pages/list';
 import Orders from './pages/Orders';
+import Login from './components/Login';
 
 
 const App = () => {
+  const [token, setToken] = useState("");
+
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-      <div >
-        <Navbar />
-        <hr />
-        <div className='flex w-full'>
-          <Sidebar />
-          <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+      {token === "" ?
+        <Login /> :
+        <div >
+          <Navbar />
+          <hr />
+          <div className='flex w-full'>
+            <Sidebar />
+            <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
               <Routes>
-                  <Route path='/add' element={<Add />}/>
-                  <Route path='/list' element={<List />}/>
-                  <Route path='/orders' element={<Orders />}/>
+                <Route path='/add' element={<Add />} />
+                <Route path='/list' element={<List />} />
+                <Route path='/orders' element={<Orders />} />
               </Routes>
+            </div>
           </div>
         </div>
-      </div>
+      }
+
     </div>
   );
 };
