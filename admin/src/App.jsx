5768f5e1e216fd97ba1afacb994e59d739fc2cd1
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-09-13 15:57:40 +0600

    Admin navbar is created

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div>
-      Admin pannel
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar />
     </div>
   );
 };
---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/logo.png'
+
+const Navbar = () => {
+    return (
+        <div className='flex items-center justify-between py-2 px-[4%]'>
+            <img className="w-[80px] min-w-[10%]" src={logo} alt="" />
+            <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+        </div>
+    );
+};
+
+export default Navbar;
