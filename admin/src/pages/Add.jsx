cf91d04c18ee81b67bdd7b407abcd4c61775257a
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-09-15 01:17:25 +0600

    Admin add page designe completed

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,10 +1,88 @@
 import React from 'react';
+import { assets } from '../assets/assets';
 
 const Add = () => {
     return (
-        <div>
-            Add page
-        </div>
+        <form action="" className='flex flex-col w-full items-start'>
+            <div>
+                <p className='mb-2'>Upload Image</p>
+                <div className='flex gap-2'>
+                    <label htmlFor="image1">
+                        <img className='w-20' src={assets.upload_area} alt="" srcset="" />
+                        <input type="file" id="image1" hidden />
+                    </label>
+                    <label htmlFor="image2">
+                        <img className='w-20' src={assets.upload_area} alt="" srcset="" />
+                        <input type="file" id="image2" hidden />
+                    </label>
+                    <label htmlFor="image3">
+                        <img className='w-20' src={assets.upload_area} alt="" srcset="" />
+                        <input type="file" id="image3" hidden />
+                    </label>
+                    <label htmlFor="image4">
+                        <img className='w-20' src={assets.upload_area} alt="" srcset="" />
+                        <input type="file" id="image4" hidden />
+                    </label>
+                </div>
+            </div>
+            <div className='w-full'>
+                <p className='mb-2'>Product name</p>
+                <input type="text" className='w-full max-w-[500px] p-2' placeholder='type here' name="" id="" required />
+            </div>
+            <div className='w-full'>
+                <p className='mb-2'>Product description</p>
+                <textarea type="text-area" className='w-full max-w-[500px] p-2' placeholder='write content here' name="" id="" required />
+            </div>
+            <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
+                <div>
+                    <p className='mb-2'>Product category</p>
+                    <select className='w-full px-3 py-2' name="" id="">
+                        <option value="Men">Men</option>
+                        <option value="Women">Women</option>
+                        <option value="Kids">Kids</option>
+                    </select>
+                </div>
+                <div>
+                    <p className='mb-2'>Product Subcategory</p>
+                    <select className='w-full px-3 py-2' name="" id="">
+                        <option value="Topwear">Topwear</option>
+                        <option value="Bottomwear">Bottomwear</option>
+                        <option value="Winterwear">Winterwear</option>
+                    </select>
+                </div>
+                <div>
+                    <p className='mb-2'>Product Price</p>
+                    <input type="Number" className='w-full px-3 py-2 sm:w-[120px]' placeholder='please enter the price' />
+                </div> 
+            </div>
+            <div>
+                    <p className='mb-2'>Product Sizes</p>
+                    <div className='flex gap-3'>
+                        <div>
+                            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>S</p>
+                        </div>
+                        <div>
+                            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>M</p>
+                        </div>
+                        <div>
+                            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>L</p>
+                        </div>
+                        <div>
+                            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
+                        </div>
+                        <div>
+                            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XXL</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className='flex gap-2 mt-2'>
+                    <input type="checkbox" name="" id="bestseller" />
+                    <label className='cursor-pointer' htmlFor="bestseller">Add to bestseller</label>
+                </div>
+
+                <button type='submit' className='w-28 py-3 mt-4 bg-black text-white'>ADD</button>
+        </form>
     );
 };
 
