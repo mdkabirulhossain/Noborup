a70f783d6f48b50f5a321c9587245eabda061315
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-09-14 18:06:59 +0600

    admin login is created with authentication

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from '../assets/logo.png'
 
-const Navbar = () => {
+const Navbar = ({setToken}) => {
     return (
         <div className='flex items-center justify-between py-2 px-[4%]'>
             <img className="w-[80px] min-w-[10%]" src={logo} alt="" />
-            <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+            <button onClick={()=> setToken('')} className='bg-gray-600 cursor-pointer text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
         </div>
     );
 };
