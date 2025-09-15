0f4c742c2e3c398d6694b563849e60350c9a5f7b
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-09-15 15:55:53 +0600

    Admin add product page is completed

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,42 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { assets } from '../assets/assets';
+import axios from 'axios'
+import { backendUrl } from '../App';
+import { toast } from 'react-toastify';
 
-const Add = () => {
+const Add = ({ token }) => {
+    const [image1, setImage1] = useState(false);
+    const [image2, setImage2] = useState(false);
+    const [image3, setImage3] = useState(false);
+    const [image4, setImage4] = useState(false);
+
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState("");
+    const [category, setCategory] = useState("Men");
+    const [subCategory, setSubCategory] = useState("Topwear");
+    const [bestseller, setBestseller] = useState(false);
+    const [sizes, setSizes] = useState([]);
+
+    const onSubmitHander = async (e) => {
+        e.preventDefault();
+
+        try {
+            const formdata = new FormData();
+            formdata.append("name", name);
+            formdata.append("description", description);
+            formdata.append("price", price);
+            formdata.append("category", category);
+            formdata.append("subCategory", subCategory);
+            formdata.append("bestseller", bestseller);
+            formdata.append("sizes", JSON.stringify(sizes));
+
+            image1 && formdata.append("image1", image1);
+            image2 && formdata.append("image2", image2);
+            image3 && formdata.append("image3", image3);
+            image4 && formdata.append("image4", image4);
+
+            // Log all FormData
+            // for (let [key, value] of formdata.entries()) {
+            //     console.log(`${key}:`, value);
+            // }
+
+            const response = await axios.post(backendUrl + "/api/product/add", formdata, { headers: { token } })
+            // console.log(response.data);
+            if (response.data.success) {
+                toast.success(response.data.message);
+                setName('');
+                setDescription('');
+                setPrice('');
+                setCategory('');
+                setSubCategory('');
+                setBestseller(false);
+                setSizes([]);
+
+                // Reset image files
+                setImage1(null);
+                setImage2(null);
+                setImage3(null);
+                setImage4(null);
+            }else{
+                toast.error(response.data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
     return (
-        <form action="" className='flex flex-col w-full items-start'>
+        <form onSubmit={onSubmitHander} action="" className='flex flex-col w-full items-start'>
             <div>
                 <p className='mb-2'>Upload Image</p>
                 <div className='flex gap-2'>
                     <label htmlFor="image1">
-                        <img className='w-20' src={assets.upload_area} alt="" srcset="" />
-                        <input type="file" id="image1" hidden />
+                        <img className='w-20 cursor-pointer ' src={!image1 ? assets.upload_area : URL.createObjectURL(image1)} alt="" />
+                        <input onChange={(e) => setImage1(e.target.files[0])} type="file" id="image1" hidden />
                     </label>
                     <label htmlFor="image2">
-                        <img className='w-20' src={assets.upload_area} alt="" srcset="" />
-                        <input type="file" id="image2" hidden />
+                        <img className='w-20 cursor-pointer' src={!image2 ? assets.upload_area : URL.createObjectURL(image2)} alt="" />
+                        <input onChange={(e) => setImage2(e.target.files[0])} type="file" id="image2" hidden />
                     </label>
                     <label htmlFor="image3">
-                        <img className='w-20' src={assets.upload_area} alt="" srcset="" />
-                        <input type="file" id="image3" hidden />
+                        <img className='w-20 cursor-pointer' src={!image3 ? assets.upload_area : URL.createObjectURL(image3)} alt="" />
+                        <input onChange={(e) => setImage3(e.target.files[0])} type="file" id="image3" hidden />
                     </label>
                     <label htmlFor="image4">
-                        <img className='w-20' src={assets.upload_area} alt="" srcset="" />
-                        <input type="file" id="image4" hidden />
+                        <img className='w-20 cursor-pointer' src={!image4 ? assets.upload_area : URL.createObjectURL(image4)} alt="" />
+                        <input onChange={(e) => setImage4(e.target.files[0])} type="file" id="image4" hidden />
                     </label>
                 </div>
             </div>
             <div className='w-full'>
                 <p className='mb-2'>Product name</p>
-                <input type="text" className='w-full max-w-[500px] p-2' placeholder='type here' name="" id="" required />
+                <input onChange={(e) => setName(e.target.value)} value={name} type="text" className='w-full max-w-[500px] p-2' placeholder='type here' name="" id="" required />
             </div>
             <div className='w-full'>
                 <p className='mb-2'>Product description</p>
-                <textarea type="text-area" className='w-full max-w-[500px] p-2' placeholder='write content here' name="" id="" required />
+                <textarea onChange={(e) => setDescription(e.target.value)} value={description} type="text-area" className='w-full max-w-[500px] p-2' placeholder='write content here' name="" id="" required />
             </div>
             <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
                 <div>
                     <p className='mb-2'>Product category</p>
-                    <select className='w-full px-3 py-2' name="" id="">
+                    <select onChange={(e) => setCategory(e.target.value)} value={category} className='w-full px-3 py-2' name="" id="">
                         <option value="Men">Men</option>
                         <option value="Women">Women</option>
                         <option value="Kids">Kids</option>
@@ -44,7 +109,7 @@ const Add = () => {
                 </div>
                 <div>
                     <p className='mb-2'>Product Subcategory</p>
-                    <select className='w-full px-3 py-2' name="" id="">
+                    <select onChange={(e) => setSubCategory(e.target.value)} value={subCategory} className='w-full px-3 py-2' name="" id="">
                         <option value="Topwear">Topwear</option>
                         <option value="Bottomwear">Bottomwear</option>
                         <option value="Winterwear">Winterwear</option>
@@ -52,36 +117,36 @@ const Add = () => {
                 </div>
                 <div>
                     <p className='mb-2'>Product Price</p>
-                    <input type="Number" className='w-full px-3 py-2 sm:w-[120px]' placeholder='please enter the price' />
-                </div> 
+                    <input onChange={(e) => setPrice(e.target.value)} value={price} type="Number" className='w-full px-3 py-2 sm:w-[120px]' placeholder='please enter the price' />
+                </div>
             </div>
             <div>
-                    <p className='mb-2'>Product Sizes</p>
-                    <div className='flex gap-3'>
-                        <div>
-                            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>S</p>
-                        </div>
-                        <div>
-                            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>M</p>
-                        </div>
-                        <div>
-                            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>L</p>
-                        </div>
-                        <div>
-                            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
-                        </div>
-                        <div>
-                            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XXL</p>
-                        </div>
-
+                <p className='mb-2'>Product Sizes</p>
+                <div className='flex gap-3'>
+                    <div onClick={() => setSizes(prev => prev.includes("S") ? prev.filter(item => item !== "S") : [...prev, "S"])}>
+                        <p className={`${sizes.includes("S") ? 'bg-pink-100' : 'bg-slate-200'} px-3 py-1 cursor-pointer`}>S</p>
+                    </div>
+                    <div onClick={() => setSizes(prev => prev.includes("M") ? prev.filter(item => item !== "M") : [...prev, "M"])}>
+                        <p className={`${sizes.includes("M") ? 'bg-pink-100' : 'bg-slate-200'} px-3 py-1 cursor-pointer`}>M</p>
+                    </div>
+                    <div onClick={() => setSizes(prev => prev.includes("L") ? prev.filter(item => item !== "L") : [...prev, "L"])}>
+                        <p className={`${sizes.includes("L") ? 'bg-pink-100' : 'bg-slate-200'} px-3 py-1 cursor-pointer`}>L</p>
+                    </div>
+                    <div onClick={() => setSizes(prev => prev.includes("XL") ? prev.filter(item => item !== "XL") : [...prev, "XL"])}>
+                        <p className={`${sizes.includes("XL") ? 'bg-pink-100' : 'bg-slate-200'} px-3 py-1 cursor-pointer`}>XL</p>
+                    </div>
+                    <div onClick={() => setSizes(prev => prev.includes("XXL") ? prev.filter(item => item !== "XXL") : [...prev, "XXL"])}>
+                        <p className={`${sizes.includes("XXL") ? 'bg-pink-100' : 'bg-slate-200'} px-3 py-1 cursor-pointer`}>XXL</p>
                     </div>
                 </div>
-                <div className='flex gap-2 mt-2'>
-                    <input type="checkbox" name="" id="bestseller" />
-                    <label className='cursor-pointer' htmlFor="bestseller">Add to bestseller</label>
-                </div>
 
-                <button type='submit' className='w-28 py-3 mt-4 bg-black text-white'>ADD</button>
+            </div>
+            <div className='flex gap-2 mt-2'>
+                <input onChange={() => setBestseller(prev => !prev)} checked={bestseller} type="checkbox" name="" id="bestseller" />
+                <label className='cursor-pointer' htmlFor="bestseller">Add to bestseller</label>
+            </div>
+
+            <button type='submit' className='w-28 cursor-pointer py-3 mt-4 bg-black text-white'>ADD</button>
         </form>
     );
 };
