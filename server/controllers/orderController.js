e77f99527b3b06ddb23d9f00581917c2f27e2d21
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-09-20 15:51:32 +0600

    PlaceOrder Controller function is done

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,8 +1,31 @@
+import userModel from "../models/userModel.js";
 
 
 // Placing orders using Cash on Delivery (COD) Methon
 const placeOrder = async(req, res) =>{
-    
+    try{
+        const {userId, items, amount, address} = req.body;
+
+        const orderData ={
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod:"COD",
+            payment: false,
+            date: Date.now()
+        }
+
+        const newOrder = new orderModel(orderData)
+        await newOrder.save()
+
+        await userModel.findByIdAndUpdate(userId, {cartData:{}})
+        res.json({success:true, message:"Order Placed"})
+
+    }catch(error){
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
 }
 
 
