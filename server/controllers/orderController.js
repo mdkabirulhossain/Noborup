a3a4f91c93998c13c0873c65943bb321a967350a
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-10-04 16:58:35 +0600

    Wrote updateOrderStatus controller function

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -64,7 +64,15 @@ const userOrders = async (req, res) => {
 
 //Update order Status
 const updateOrdersStatus = async (req, res) => {
+    try{
+        const {orderId, status} = req.body;
+        await orderModel.findByIdAndUpdate(orderId, {status})
+        res.json({success:true, message:"Status Updated"})
 
+    }catch(error){
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 }
 
 
